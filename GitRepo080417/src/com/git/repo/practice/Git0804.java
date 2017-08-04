a258cc544de9406353bcafe40763c8720db6132e
kadange <gskadano@gmail.com>
2017-08-04 11:59:15 +0800

    Added zipcode

--- GitRepo080417/src/com/git/repo/practice/Git0804.java
+++ GitRepo080417/src/com/git/repo/practice/Git0804.java
@@ -7,11 +7,13 @@ public class Git0804 {
 		String myEmail = "gskadano@gmail.com";
 		String myAddress = "Quezon City, Philippines";
 		String myPhone = "09178646304";
+		int zipCode = 12345;
 		
 		System.out.println(myname);
 		System.out.println(myEmail);
 		System.out.println(myAddress);
 		System.out.println(myPhone);
+		System.out.println("Zip code:"+zipCode);
 		System.out.println("===============");
 		
 		for(int i=0;i<5;i++){
