1db8d06a9e3ef26f34bca2c5b665c1b5d1942368
kadange <gskadano@gmail.com>
2017-08-04 11:27:57 +0800

    First commit

---
+++ GitRepo080417/src/com/git/repo/practice/Git0804.java
@@ -0,0 +1,17 @@
+package com.git.repo.practice;
+
+public class Git0804 {
+
+	public static void main(String[] args) {
+		String myname = "Gene Anthony";
+		String myEmail = "gskadano@gmail.com";
+		String myAddress = "Quezon City, Philippines";
+//		long myPhone = 09178646304L;
+		
+		System.out.println(myname);
+		System.out.println(myEmail);
+		System.out.println(myAddress);
+
+	}
+
+}
